ffdb67ef871475118d8350f69840d8550effcb86
shivaskj <shivamconnects@gmail.com>
2025-04-07 14:42:07 +0530

    mt1

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = { 
-  title: 'ChatUI',
+  title: 'ChatUI main test 1',
   description: 'A chat application',
 }
 
